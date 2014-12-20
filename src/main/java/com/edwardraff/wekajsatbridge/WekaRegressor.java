44d2ea39f4c630812d3623eed314dd782c0f72a4
EdwardRaff <Raff.Edward@gmail.com>
2014-12-19 21:39:14 -0500

    Merge origin/master
    Conflicts:
    	src/main/java/com/edwardraff/wekajsatbridge/WekaClassifier.java
    	src/main/java/com/edwardraff/wekajsatbridge/WekaRegressor.java

--- src/main/java/com/edwardraff/wekajsatbridge/WekaRegressor.java
+++ src/main/java/com/edwardraff/wekajsatbridge/WekaRegressor.java
@@ -17,6 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+package com.edwardraff.wekajsatbridge;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
